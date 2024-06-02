0aa9fe5784d614c9b19a45a5e33009854a378847
Jahan511 <jahangirhussain9694@gmail.com>
2024-06-02 17:26:18 +0500

    latest

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -5,7 +5,7 @@ import Logo from "../assets/logo.png";
 import { Mail, Lock, Eye, EyeOff } from 'tabler-icons-react';
 
 function Signin() {
-  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(true);
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
@@ -13,64 +13,65 @@ function Signin() {
 
   return (
     <>
-    <div style={{display:"flex",alignItems:"center" , flexWrap:"wrap",gap:5 } } className="login-container2">
-      <div className="image-container"  >
-        <img src={buildingImage} alt="Building"  height={'600px'} />
-      </div>
-
-      <div className="login-form">
-        <div className="signin">
-          <img src={Logo} alt="Logo" />
-          <h1>Sign In to your account</h1>
-          <h3 style={{marginLeft:"10px"}} className="signin-subtext">
-          Enter your details to proceed further
-        </h3>
+      <div style={{ display: "flex", alignItems: "center", flexWrap: "wrap", gap: 5 }} className="login-container2">
+        <div className="image-container">
+          <img src={buildingImage} alt="Building" height={'600px'} />
         </div>
-       
 
-        <form>
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <div className="input-with-icon">
-            <Mail className="input-icon" />
-            <input type="email" id="email" placeholder="Enter your email" />
+        <div className="login-form">
+          <div className="signin">
+            <img src={Logo} alt="Logo" />
+            <h1>Sign In to your account</h1>
+            <h3 style={{ marginLeft: "10px" }} className="signin-subtext">
+              Enter your details to proceed further
+            </h3>
+          </div>
+
+          <form>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <div className="input-with-icon">
+                <Mail className="input-icon" />
+                <input type="email" id="email" placeholder="Enter your email" />
+              </div>
             </div>
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <div className="input-with-icon"> <Lock className="input-icon" />
-            <input
-              type={passwordVisible ? "text" : "password"}
-              id="password"
-              placeholder="Enter your password"
-            />
-            
-          </div>
-          </div>
-          <div className="forgot-password">
-            <a href="/forgetpassword">Forget Password?</a>
-          </div>
-          <button type="submit">Sign In</button>
-        </form>
-        <p className="terms">
-          By logging in, you are agreeing to our{" "}
-          <a href="#">Terms & Conditions</a>.
-        </p>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <div className="input-with-icon">
+                <Lock className="input-icon" />
+                <input
+                  id="password"
+                  type={passwordVisible ? "text" : "password"}
+                  placeholder="Enter your password"
+                />
+                <div className="password-toggle" onClick={togglePasswordVisibility}>
+                  {passwordVisible ? <Eye /> : <EyeOff />}
+                </div>
+              </div>
+            </div>
+            <div className="forgot-password">
+              <a href="/forgetpassword">Forget Password?</a>
+            </div>
+            <button type="submit">Sign In</button>
+          </form>
+          <p className="terms">
+            By logging in, you are agreeing to our{" "}
+            <a href="#">Terms & Conditions</a>.
+          </p>
+        </div>
       </div>
-     
-    </div>
-     <div className="footer">
-     <p>© Copyright 2008 - 2024 Carview. All Rights Reserved</p>
+      <div className="footer">
+        <p>© Copyright 2008 - 2024 Carview. All Rights Reserved</p>
 
-     <div className="footer-links">
-     <a href="#"></a>
-       <a href="#">Terms & Conditions</a>
-       <a href="#">Privacy Policy</a>
-       <a href="#">Earnings Disclaimer</a>
-       <a href="#">Compensation Disclosure</a>
-     </div>
-   </div>
-   </>
+        <div className="footer-links">
+        <a href="#"></a>
+          <a href="#">Terms & Conditions</a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Earnings Disclaimer</a>
+          <a href="#">Compensation Disclosure</a>
+        </div>
+      </div>
+    </>
   );
 }
 
